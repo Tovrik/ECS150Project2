c32be44776c5b2c4200efd49cf4367a10ecca217
shaibruhis <shaibruhis@gmail.com>
2015-04-22 11:53:30 -0700

    got hello.so working. To run it you first need to type 'make apps' this will convert .c files to .so files. Also when you run it you need to type ./ before app name so it'll be: ./vm ./hello.so blah1 blah2 blah3

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -5,6 +5,57 @@
 
 // using namespace std;
 
+
+
+extern "C" {
+TVMMainEntry VMLoadModule(const char *module);
+
+
+TVMStatus VMFileClose(int filedescriptor) {
+    if (close(filedescriptor) == 0) {
+        return VM_STATUS_SUCCESS;
+    }
+    else {
+        return VM_STATUS_FAILURE;
+    }
+
+}
+
+// void MachineFileOpen(const char *filename, int flags, int mode, TMachineFileCallback callback, void *calldata);
+
+
+// TVMStatus VMFileOpen(const char *filename, int flags, int mode, int *filedescriptor) {
+//     if (filename == NULL || filedescriptor == NULL) {
+//         return VM_STATUS_ERROR_INVALID_PARAMETER;
+//     }
+//     else {
+//         MachineFileOpen(filename, flags, mode, ___, ___);
+//         *filedescriptor = open(filename, flags, mode);
+//         if (*filedescriptor != -1) {
+//             return VM_STATUS_SUCCESS;
+//         }
+//         else {
+//             return VM_STATUS_FAILURE;
+//         }
+//     }
+// }
+
+
+TVMStatus VMFileWrite(int filedescriptor, void *data, int *length) {
+    //VMFileOpen(, &filedescriptor);
+    if (data == NULL || length == NULL) {
+        return VM_STATUS_ERROR_INVALID_PARAMETER;
+    }
+    else {
+        if (write(filedescriptor, (char *)data, *length) != -1) {
+            return VM_STATUS_SUCCESS;
+        }
+        else {
+            return VM_STATUS_FAILURE;
+        }
+    }
+}
+
 // VMStart
 // -Use typedef to define a function pointer and then create an instance/variable of that function pointer type
 // -Then set that variable equal to the address returned from VMLoadModule
@@ -13,12 +64,6 @@
 // -Then you'll want to check which functions are called in the hello.c
 // -You'll notice that it in hello.c it calls VMPrint
 // -So get VMPrint to work using some system calls and error handling
-
-extern "C" {
-TVMMainEntry VMLoadModule(const char *module);
-
-
-
 TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[]) {
     typedef void (*TVMMainEntry)(int argc, char* argv[]);
     TVMMainEntry VMMain;
@@ -31,9 +76,4 @@ TVMStatus VMStart(int tickms, int machinetickms, int argc, char *argv[]) {
         return VM_STATUS_FAILURE;
     }
 }
-
-
-
-
-
 } // end extern C
